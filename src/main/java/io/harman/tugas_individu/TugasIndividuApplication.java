4cd6a4965de9bd10f1d12a2a38e970a5fe6df594
Harman Hakim <harman.hakim@ui.ac.id>
2025-10-25 18:25:45 +0700

    init

---
+++ src/main/java/io/harman/tugas_individu/TugasIndividuApplication.java
@@ -0,0 +1,13 @@
+package io.harman.tugas_individu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasIndividuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasIndividuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/harman/tugas_individu/TugasIndividuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harman.tugas_individu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasIndividuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
