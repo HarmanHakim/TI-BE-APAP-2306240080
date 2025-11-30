81d0579adb154738dbc545f41e5f15fecb4f7ee2
Harman Hakim <harman.hakim@ui.ac.id>
2025-12-01 03:02:29 +0700

    fix: permit all healtcheck

--- src/main/java/io/harman/flight_be/security/WebSecurityConfig.java
+++ src/main/java/io/harman/flight_be/security/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/loyalty/points", "/api/loyalty/coupons/redeem").permitAll() // Handled by
-                                                                                                           // ApiKeyFilter
+                        .requestMatchers("/api/health").permitAll() 
                         .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated());
